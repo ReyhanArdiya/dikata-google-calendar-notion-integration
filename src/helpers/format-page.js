4c5b2cc5b06dc80927c4561643d69eaf618fd464
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-03-31 15:43:43 +0700

    Create `formatPage` helper

---
+++ src/helpers/format-page.js
@@ -0,0 +1,40 @@
+/**
+ * Formats a notion page's JSON data from the database API into another JSON format.
+ *
+ * @param {any} page A notion page's JSON data from the database API
+ *
+ * @returns {{
+ *date: {start: string | null, end: string | null, time_zone: string | null},
+ *id: string,
+ * name: string,
+ *progress: string,
+ *summary: string,
+ *type: string,
+ *url: string
+ *}}
+ */
+const formatPage = page => {
+	const {
+		id,
+		properties : {
+			Type: { select: { name: type } },
+			Progress: { select: { name: progress } },
+			Date: { date },
+			Summary: { "rich_text": [ { "plain_text": summary } ] },
+			Name: { title: [ { "plain_text": name } ] }
+		},
+		url
+	} = page;
+
+	return {
+		date,
+		id,
+		name,
+		progress,
+		summary,
+		type,
+		url
+	};
+};
+
+export default formatPage;
