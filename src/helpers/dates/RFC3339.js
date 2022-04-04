b8ec0f44473ff7d11e760a28fc932e51ab75dba7
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-04-04 14:22:52 +0700

    Add option to throw error in `validateRFC3339`

--- src/helpers/dates/RFC3339.js
+++ src/helpers/dates/RFC3339.js
@@ -3,8 +3,11 @@
  *
  * @param {string} date A RFC3339 timestamp with or without time zone offset.
  *
+ * @param {boolean} throwErr
+ *
+ * @throws if {@link throwErr} is true and the date string is invalid.
  */
-export const validateRFC3339 = date => {
+export const validateRFC3339 = (date, throwErr = false) => {
 	// Year must be 4 characters and there is no limit
 	const YY = "[0-9]{4}";
 	// Month must be from 00-12
@@ -21,8 +24,13 @@ export const validateRFC3339 = date => {
 	const TZ = "(Z|[-,+][0-9]{2}:[0-9]{2})?";
 
 	const rfc3339Format = new RegExp(`^${YY}-${MM}-${DD}(T${HH}:${MMinute}:${SS}${TZ})?$`);
+	const testRes = rfc3339Format.test(date);
 
-	return rfc3339Format.test(date);
+	if (throwErr && !testRes) {
+		throw new Error("Invalid date! date must be formatted in \"YYYY-MM-DD\" or \"YYYY-MM-DDTHH:MM:SS\" and must not exceed each time's limit, e.g. HH can't be more than 23.");
+	}
+
+	return testRes;
 };
 
 /**
@@ -72,9 +80,7 @@ export const createRFC3339 = (YY, MM, DD, HH = "", MMinute = "", SS = "", TZ = "
 		YYMMDD += TZ;
 	}
 
-	if (!validateRFC3339(YYMMDD)) {
-		throw new Error("Invalid date! date must be formatted in \"YYYY-MM-DD\" or \"YYYY-MM-DDTHH:MM:SS\" and must not exceed each time's limit, e.g. HH can't be more than 23.");
-	}
+	validateRFC3339(YYMMDD, true);
 
 	return YYMMDD;
 };
