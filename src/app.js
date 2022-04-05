ca2d0c176ab3f33eec0e06d1c70321d77fb55beb
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-04-05 15:30:34 +0700

    Connect to mongo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import checkGcalEnvTokens from "./helpers/google-calendar/tokens/check-gcal-env-tokens.js";
+import mongoose from "mongoose";
 
 if (process.env.NODE_ENV !== "production") {
 	(await import("dotenv")).config();
@@ -9,3 +10,10 @@ if (!checkGcalEnvTokens()) {
 	await import("./helpers/google-calendar/tokens/get-token.js");
 }
 
+const mongoDatabase = process.env.MONGODB;
+try {
+	await mongoose.connect(mongoDatabase);
+	console.log(`Connected to ${mongoDatabase}!🍃`);
+} catch (err) {
+	console.log(`Error! Can't connect to ${mongoDatabase}!🍂`, err);
+}
