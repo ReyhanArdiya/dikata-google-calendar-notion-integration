8a260d0cd98ed1f4546dab4b71fdcb4e2c47cc46
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-04-07 10:07:15 +0700

    Upgraded `Watcher` class

--- src/helpers/watchers/watcher.js
+++ src/helpers/watchers/watcher.js
@@ -1,56 +1,82 @@
 /**
- * A class where you can pass and manipulate how a callback is called
- * based on an interval.
+ * A class where you can pass an async `watchingFn` that will be called and
+ * the awaited result will be passed to `asyncCb` as the first and only argument
+ * for every `ms` interval.
  */
 class Watcher {
-	#callback = null;
+	#watchingFn = null;
+	#asyncCb = null;
 	#ms = null;
 	#intervalTimer = null;
 
-	constructor(callback, ms) {
-		this.#callback = callback;
+	/**
+	 * When this method is called, {@link watchingFn} will be called
+	 * and awaited; then it will call {@link asyncCb} with the awaited result passed
+	 * in as the first and only argument.
+	 *
+	 */
+	#watch = async () => await this.#asyncCb(await this.#watchingFn());
+
+	/**
+	 *
+	 * @param {() => Promise<any>} watchingFn
+	 *
+	 * @param {(watchingFnRes : any) => Promise<any>} asyncCb
+	 *
+	 * @param {number} ms
+	 */
+	constructor(watchingFn, asyncCb, ms) {
+		this.#watchingFn = watchingFn;
+		this.#asyncCb = asyncCb;
 		this.#ms = ms;
 	}
 
 	/**
 	 * Activates this {@link Watcher} by setting an interval that will call
-	 * the {@link callback} function every {@link ms} milliseconds.
+	 * the `watch` method every {@link ms}.
+	 * - If this {@link Watcher} is already activated, this method returns `false` and
+	 * does not set a new interval.
+	 * - If this method successfully activates this {@link Watcher}, it returns
+	 * this {@link Watcher}'s {@link intervalTimer}
 	 *
-	 * @param {boolean} doOnce If true, calls the {@link callback} function once
+	 * @param {boolean} doOnce If true, calls the `watch` method once
 	 * on startup before setting the interval.
-	 *
 	 */
-	activate(doOnce = false) {
-		if (doOnce) {
-			this.#callback();
+	async activate(doOnce = false) {
+		if (this.#intervalTimer) {
+			return false;
 		}
 
-		const intervalTimer = setInterval(this.#callback, this.#ms);
+		if (doOnce) {
+			await this.#watch();
+		}
+
+		const intervalTimer = setInterval(this.#watch, this.#ms);
 		this.#intervalTimer = intervalTimer;
 		return intervalTimer;
 	}
 
 	/**
 	 * Deactivates this {@link Watcher}.
+	 * - If there is no timer, this method returns `false`.
+	 * - If this method successfully deactivates this {@link Watcher}, it returns
+	 * `true`.
 	 *
-	 * @param {boolean} doOnce If true, calls the {@link callback} function once
+	 * @param {boolean} doOnce If true, calls the `watch` method once
 	 * before deactivating the `Watcher`.
 	 */
-	deactivate(doOnce = false) {
+	async deactivate(doOnce = false) {
+		if (!this.#intervalTimer) {
+			return false;
+		}
+
 		if (doOnce) {
-			this.#callback();
+			await this.#watch();
 		}
 
 		clearInterval(this.#intervalTimer);
+		this.#intervalTimer = null;
 		return true;
-	}
-
-	get callback() {
-		return this.#callback;
-	}
-
-	set callback(callback) {
-		this.#callback = callback;
 	}
 
 	get ms() {
@@ -59,6 +85,22 @@ class Watcher {
 
 	set ms(ms) {
 		this.#ms = ms;
+	}
+
+	get watchingFn() {
+		return this.#watchingFn;
+	}
+
+	set watchingFn(watchingFn) {
+		this.#watchingFn = watchingFn;
+	}
+
+	get asyncCb() {
+		return this.#asyncCb;
+	}
+
+	set asyncCb(asyncCb) {
+		this.#asyncCb = asyncCb;
 	}
 
 	get intervalTimer() {
